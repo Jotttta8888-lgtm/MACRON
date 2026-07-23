0e8e61d4f8b7bc307b7f21c448bd96258e2eff22
Jotttta8888-lgtm <jotttta8888@gmail.com>
2026-07-23 16:54:15 -0500

    Fix MACRON_v4 Swift Package structure: correct Package.swift, remove Resources

--- MACRON_v4/Package.swift
+++ MACRON_v4/Package.swift
@@ -1,8 +1,13 @@
 // swift-tools-version:5.9
 import PackageDescription
+
 let package = Package(
     name: "MACRON",
     platforms: [.macOS(.v14)],
-    products: [.executable(name: "MACRON", targets: ["MACRON"])],
-    targets: [.executableTarget(name: "MACRON", path: "Sources/MACRON")]
+    products: [
+        .executable(name: "MACRON", targets: ["MACRON"])
+    ],
+    targets: [
+        .executableTarget(name: "MACRON")
+    ]
 )
