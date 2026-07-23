5ecf10779e0c80b55e8858a03d1a8425f3df3ec7
Jotttta8888-lgtm <jotttta8888@gmail.com>
2026-07-23 17:02:11 -0500

    Add MACRON_v4 Swift Package with correct structure

---
+++ MACRON_v4/Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.9
+import PackageDescription
+
+let package = Package(
+    name: "MACRON",
+    platforms: [.macOS(.v14)],
+    products: [
+        .executable(name: "MACRON", targets: ["MACRON"])
+    ],
+    targets: [
+        .executableTarget(name: "MACRON")
+    ]
+)
---
+++ MACRON_v4/Sources/MACRON/FocusTextField.swift
@@ -0,0 +1,60 @@
+import SwiftUI
+import AppKit
+
+class FocusTextField: NSTextField {
+    override func viewDidMoveToWindow() {
+        super.viewDidMoveToWindow()
+        self.window?.makeFirstResponder(self)
+    }
+}
+
+struct NativeTextField: NSViewRepresentable {
+    @Binding var text: String
+    var placeholder: String
+    var onSubmit: (() -> Void)?
+    
+    func makeNSView(context: Context) -> NSTextField {
+        let tf = FocusTextField()
+        tf.placeholderString = placeholder
+        tf.delegate = context.coordinator
+        tf.bezelStyle = .roundedBezel
+        tf.isEditable = true
+        tf.isSelectable = true
+        tf.isEnabled = true
+        tf.allowsEditingTextAttributes = true
+        tf.font = NSFont.systemFont(ofSize: NSFont.systemFontSize)
+        tf.stringValue = text
+        return tf
+    }
+    
+    func updateNSView(_ nsView: NSTextField, context: Context) {
+        if nsView.stringValue != text {
+            nsView.stringValue = text
+        }
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, NSTextFieldDelegate {
+        var parent: NativeTextField
+        
+        init(_ parent: NativeTextField) {
+            self.parent = parent
+        }
+        
+        func controlTextDidChange(_ obj: Notification) {
+            guard let tf = obj.object as? NSTextField else { return }
+            parent.text = tf.stringValue
+        }
+        
+        func control(_ control: NSControl, textView: NSTextView, doCommandBy commandSelector: Selector) -> Bool {
+            if commandSelector == #selector(NSResponder.insertNewline(_:)) {
+                parent.onSubmit?()
+                return true
+            }
+            return false
+        }
+    }
+}
